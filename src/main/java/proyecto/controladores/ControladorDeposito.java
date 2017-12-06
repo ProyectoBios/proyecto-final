37ee061e92045079bd989e485dbb17ae4215bcaf
Alvaro <alvaromar2008@gmail.com>
2017-12-06 16:20:14 -0300

    - Se establecieron algunos placeholders para thymeleaf"

--- src/main/java/proyecto/controladores/ControladorDeposito.java
+++ src/main/java/proyecto/controladores/ControladorDeposito.java
@@ -1,13 +1,26 @@
 package proyecto.controladores;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import proyecto.datatypes.DTEspecificacionProducto;
+import proyecto.datatypes.DTPrecio;
+
+import java.util.ArrayList;
 
 @Controller
 public class ControladorDeposito {
     @RequestMapping("/")
     public String prueba(){
         return "index";
+    }
+
+    @RequestMapping(value="/ABMProducto", method = RequestMethod.GET)
+    public String AbmProducto(ModelMap modelMap){
+        modelMap.addAttribute("producto", new DTEspecificacionProducto());
+        return "ABMProducto";
     }
 
 }
