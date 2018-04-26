a872e6bfd81252b9a0e62215e02812734e29ffdb
Alvaro <alvaromar2008@gmail.com>
2018-04-26 16:38:01 -0300

    - Se mejoró la interfaz gráfica del botón desplegable.

--- src/main/resources/static/assets/js/ddlFiltrable.js
+++ src/main/resources/static/assets/js/ddlFiltrable.js
@@ -1,12 +1,17 @@
 function desplegarDDL() {
-    document.getElementById("myDropdown").classList.toggle("show");
+    if(document.getElementById("desplegable").classList.toggle("show")){
+        document.getElementById("btnSeleccionar").className="dropbtn flechaArriba";
+    }else{
+        document.getElementById("btnSeleccionar").className="dropbtn flechaAbajo";
+    } /* acá debería ir una función para el "onclick" para que se muestre u oculte con el click */
+
 }
 
 function filtrarDDL() {
     var input, filter, ul, li, a, i;
-    input = document.getElementById("myInput");
+    input = document.getElementById("entrada");
     filter = input.value.toUpperCase();
-    div = document.getElementById("myDropdown");
+    div = document.getElementById("desplegable");
     a = div.getElementsByTagName("a");
     for (i = 0; i < a.length; i++) {
         if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
@@ -17,6 +22,21 @@ function filtrarDDL() {
     }
 }
 
-function seleccionarValor(dato) {
-    document.getElementById("idDropdown").value = dato;
+function seleccionarValor(codigo, nombre) {
+    document.getElementById("idDropdown").value = codigo;
+    document.getElementById("btnSeleccionar").innerHTML = nombre;
 }
+
+$(document).click(function () {
+    document.getElementById("desplegable").classList.remove("show");
+    document.getElementById("btnSeleccionar").className="dropbtn flechaAbajo";
+
+})
+
+$("#btnSeleccionar").click(function (e) {
+    e.stopPropagation();
+})
+
+$("#entrada").click(function (e) {
+    e.stopPropagation();
+})
